ceebb36fc6deda1f369681b2f7fc1c97fff6759b
Minuri Sewmini <minurisewmini11@gmail.com>
2026-03-10 18:48:50 +0530

    Fixed error

--- mini-travel-frontend/src/api.js
+++ mini-travel-frontend/src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'mini-travel-app-production.up.railway.app/api' });
+const API = axios.create({ baseURL: 'mini-travel-app-production.up.railway.app' });
 
 export default API;
