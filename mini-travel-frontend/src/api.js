82ba664628fb6c934c5a9906727ec268dc895002
Minuri Sewmini <minurisewmini11@gmail.com>
2026-03-10 18:45:29 +0530

    Change the baseURL

--- mini-travel-frontend/src/api.js
+++ mini-travel-frontend/src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'mini-travel-app-production.up.railway.app/api' });
 
 export default API;
